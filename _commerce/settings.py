e222b29102fcb8d61ebe9876fc417ae14bfd3763
Pedro26fer <p2698fernandes@gmail.com>
2022-10-27 11:09:34 -0300

    heroku config
    Signed-off-by: Pedro26fer <p2698fernandes@gmail.com>

--- _commerce/settings.py
+++ _commerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['_commerce.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://komercio-online.herokuapp.com/', 'localhost']
 
 
 # Application definition
